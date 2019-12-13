a64169d099464eb0b50a445785a778d85d13ae18
tyj20150606 <58799427+tyj20150606@users.noreply.github.com>
2019-12-13 12:08:46 +0800

    Create test.java

---
+++ src/main/java/com/zking/test.java
@@ -0,0 +1,15 @@
+package com.zking;
+
+/**
+ * Hello world!
+ *
+ */
+public class test 
+{
+    public static void main( String[] args )
+    {
+        char ser = '男';
+        String name ="admin";
+        System.out.println( "Hello World!" );
+    }
+}
