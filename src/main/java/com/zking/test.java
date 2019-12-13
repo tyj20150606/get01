f0474341e184e6da7f5fbfc15ce87f300c564131
tyj20150606 <2248083697@qq.com>
2019-12-13 14:28:33 +0800

    提交人：tyj

--- src/main/java/com/zking/test.java
+++ src/main/java/com/zking/test.java
@@ -9,7 +9,6 @@ public class test
     public static void main( String[] args )
     {
         char ser = '男';
-        String name ="admin";
         System.out.println( "Hello World!" );
     }
 }
